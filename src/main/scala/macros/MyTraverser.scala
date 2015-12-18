1c4d37eead4ceb821607e8662dec73b160a0b636
Eniwder <akabudou0120@yahoo.co.jp>
2015-12-18 18:08:06 +0900

    prettyPrintを見やすくした

--- src/main/scala/macros/MyTraverser.scala
+++ src/main/scala/macros/MyTraverser.scala
@@ -17,13 +17,20 @@ object MyTraverser {
     }
     visible = true
   }
+
+  // tpt = type tag ?
+  // rhs = right-head side(右辺)
+  // tparam = type parameter ?
+  // vparam = value parameter ?
+
   def prettyPrint(tree: Any, sp: Int = 0) {
     //   val ui = new UI(showRaw(tree))
     println()
     tree match {
       case block@Block(stats, expr) => // exprはいらない？
         println("--- enter Block ---", block.pos)
-        println(showRaw(stats))
+        //      println(showRaw(stats))
+        println(s"line: ${block.pos.line} range: ${block.pos.column - (block.pos.point - block.pos.start) - 1} - ${block.pos.end - block.pos.start}")
         stats.foreach(prettyPrint(_, sp + 1))
       //        prettyPrint(stats)
       case list@List(xs) =>
@@ -32,38 +39,42 @@ object MyTraverser {
 
       case classDef@ClassDef(mods, name, tparams, impl) =>
         println("--- enter ClassDef ---", classDef.pos)
-        println(s"mod $mods")
+        //   println(s"mod $mods")
         println(s"tName $name")
-        println(s"tDef $tparams")
-        println(s"template ${showRaw(impl)}")
+        println(s"line: ${classDef.pos.line} range: ${classDef.pos.column - (classDef.pos.point - classDef.pos.start) - 1} - ${classDef.pos.end - classDef.pos.start}")
+        //    println(s"tDef $tparams")
+        //    println(s"template ${showRaw(impl)}")
         prettyPrint(impl, sp + 1)
 
       case template@Template(parents, self, body) =>
         println("--- enter Template ---", template.pos)
-        println(s"mod $parents")
-        println(s"tName $self")
-        println(s"tDef ${showRaw(body)}")
+        //  println(s"mod $parents")
+        //   println(s"tName $self")
+        println(s"line: ${template.pos.line} range: ${template.pos.column - (template.pos.point - template.pos.start) - 1} - ${template.pos.end - template.pos.start}")
+        //  println(s"tDef ${showRaw(body)}")
         body.foreach(prettyPrint(_, sp + 1))
 
       case defDef@DefDef(mods, name, tparams, vparamss, tpt, rhs) =>  // def x = ??? (val x = ???/getter/setter)
-        println("--- enter DefDef ---", defDef.pos, defDef.pos.start, defDef.pos.end)
-        println(s"mods $mods")
-        println(s"name $name")
-        println(s"tparams $tparams")
-        println(s"vparamss $vparamss")
-        println(s"tpt $tpt", tpt.pos)
-        println(s"rhs $rhs", rhs.pos)
-      //          println(s"tDef ${showRaw(body)}")
+        println("--- enter DefDef ---", s"[$name]")
+        //      println(s"mods $mods")
+        val prefix = if (defDef.pos.point - defDef.pos.start == 0) " // " else ""
+        println(s"$prefix tparams  : $tparams")
+        println(s"$prefix vparamss : $vparamss")
+        println(s"$prefix tpt      : $tpt")
+        println(s"$prefix rhs      : $rhs")
+        println(s"$prefix line     : ${defDef.pos.line} range: ${defDef.pos.column - (defDef.pos.point - defDef.pos.start) - 1} - ${defDef.pos.end - defDef.pos.start}")
+      //    println(s"tDef ${showRaw(defDef)}")
 
       case valDef@ValDef(mods, name, tpt, rhs) => // val x = ??? or var x = ???
-        println("--- enter ValDef ---", "pos:"+valDef.pos,"focus:"+valDef.pos.focus,"point:"+valDef.pos.point,
-          "column:"+valDef.pos.column,"line:"+valDef.pos.line, "start:"+valDef.pos.start, "end:"+valDef.pos.end,"focus:"+valDef.pos.focus,"???"+
-            (valDef.pos.column-(valDef.pos.point-valDef.pos.start)-1),"SOP"+valDef.pos.startOrPoint)
-        println(s"mods $mods")
-        println(s"name $name")
-        println(s"tpt $tpt", tpt.pos)
-        println(s"rhs $rhs", rhs.pos)
-        println(show(valDef))
+        println("--- enter ValDef ---", s"[$name]")
+//        println("--- enter ValDef ---", "pos:" + valDef.pos, "focus:" + valDef.pos.focus, "point:" + valDef.pos.point,
+//          "column:" + valDef.pos.column, "line:" + valDef.pos.line, "start:" + valDef.pos.start, "end:" + valDef.pos.end, "focus:" + valDef.pos.focus, "???" +
+//            (valDef.pos.column - (valDef.pos.point - valDef.pos.start) - 1), "SOP" + valDef.pos.startOrPoint)
+        //       println(s"mods $mods")
+        println(s"tpt  : $tpt")
+        println(s"rhs  : $rhs")
+        println(s"line : ${valDef.pos.line} range: ${valDef.pos.column - (valDef.pos.point - valDef.pos.start) - 1} - ${valDef.pos.end - valDef.pos.start}")
+      //    println(showRaw(valDef))
 
       case x =>
         println("--- enter Default ---", x)
--- src/main/scala/macros/Test.scala
+++ src/main/scala/macros/Test.scala
@@ -2,6 +2,21 @@ class Test {
   private val im = "immutable"
     var vr = 123
 
+  var notInit:Int = _
+
+  val plusVal:Int =  vr + 5
+
+  val func1 = (s:String) => s*3
+  val func2 = (s:String,n:Int) => s*n
+  def func3(s:String,n:Int,f:(String,Int) => String):String = f(s,n)
+
   def doubleInt(x: Int): Int =
     x * 2
+
+  implicit val impInt = 5
+
+  def curriedFunc(one:Int)(two1:String,two2:String) = two1 * one + two2
+
+  val partialApplied = curriedFunc(5) _
+
 }
